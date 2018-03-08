fb13543f5e351ae47aea3b5039908874ffe1963c
Eragon00 <185676324@qq.com>
2018-03-08 13:52:14 +0800

    detail重复添加

--- Creatily/src/app/app.module.ts
+++ Creatily/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HttpModule } from '@angular/http';
 import { RootRouter } from './router/router';
 import { HttpService } from './utils/http.service';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NZ_MESSAGE_CONFIG } from 'ng-zorro-antd';
+
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -72,7 +74,7 @@ import { UserComponent } from './component/user/user.component';
     FormsModule,
     NgZorroAntdModule.forRoot()
   ],
-  providers: [HttpService],
+  providers: [HttpService, { provide: NZ_MESSAGE_CONFIG, useValue: { nzMaxStack: 1 } }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

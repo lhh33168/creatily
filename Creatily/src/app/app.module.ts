0fd632ed6393391a8b9a9a7cc84a82850876d46e
Dayse_Han <1320710342@qq.com>
2018-03-08 17:15:58 +0800

    search

--- Creatily/src/app/app.module.ts
+++ Creatily/src/app/app.module.ts
@@ -40,7 +40,7 @@ import { CommentComponent } from './component/comment/comment.component';
 import { RegisterpwdComponent } from './component/registerpwd/registerpwd.component';
 import { UserComponent } from './component/user/user.component';
 import { UsersetComponent } from './component/userset/userset.component';
-import { SearchComponent } from './src/app/components/search/search.component';
+import { SearchComponent } from './component/search/search.component';
 
 
 

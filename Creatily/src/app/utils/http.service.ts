816d28e3cce1e7a5827914e7b7d97b29ba3e6b1b
Jarhowe <305764766@qq.com>
2018-03-17 15:54:28 +0800

    项目打包

--- Creatily/src/app/utils/http.service.ts
+++ Creatily/src/app/utils/http.service.ts
@@ -6,7 +6,7 @@ import { Http, Headers, RequestOptions, RequestMethod } from '@angular/http';
 export class HttpService {
     constructor(private http: Http) {}
 
-    private baseUrl: String = 'http://www.creatily.top:80/';
+    private baseUrl: String = 'http://creatily.top:88/';
     private getUrl(_url) {
         if (_url.startsWith('http')) {
             return _url;
